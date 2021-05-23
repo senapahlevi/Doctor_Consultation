0ab5c51be6ba1258fb1afb05e577d79388761fe4
senapahlevi <senapahlevi1@gmail.com>
2021-05-23 21:01:47 +0700

    Explain usestate save form 1

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,12 +1,24 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useState } from 'react';
 import {ScrollView, StyleSheet, View } from 'react-native';
 import {Button, Gap, Header,Input} from '../../components';
 import { colors } from '../../utils';
-const Register = ({navigation}) => {   
-    //                
-    
+const Register = ({navigation}) => {
+    const [fullName,setFullname] = useState('');
+    const [profession, setProfession] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    /*
+    penjelasan usestate
+    nah ini kita mau cek fullname,profesion,
+    email,pass tapi kita contoh ke klik continue
+    on debug di hp kemudian inspect elemen lalu
+    console nah klik abis tuh muncul 
+    */
+    const onContinue = () =>{
+        console.log("Masuk Klik Continue")
+    };
     return (
         <View style={styles.page}>
             <Header onPress={() =>navigation.goBack()} title="Daftar Akun"/>
@@ -20,10 +32,9 @@ const Register = ({navigation}) => {
                     <Gap height={24} />
                     <Input label="password" />
                     <Gap height={24} />
-                    <Button title="Continue" onPress={()=> navigation.navigate('UploadPhoto')} />
+                    <Button title="Continue" onPress={onContinue} />
                     <Gap height={40} />
                 </ScrollView>
-            
             </View>
         </View>
     )
