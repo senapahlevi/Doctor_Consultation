5948018f2075bf22821621529a4fde648d954467
senapahlevi <senapahlevi1@gmail.com>
2021-05-24 21:33:05 +0700

    Setup Firebase Console

---
+++ src/config/fire/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    
+  // Your web app's Firebase configuration
+    apiKey: "AIzaSyBIrZLgVSQOlNQGxrwpm0EhTCMgSn-WImI",
+    authDomain: "doctor-consult-d6bdf.firebaseapp.com",
+    projectId: "doctor-consult-d6bdf",
+    storageBucket: "doctor-consult-d6bdf.appspot.com",
+    messagingSenderId: "198439298188",
+    appId: "1:198439298188:web:de2ac8380f1d03427247a9"
+  // Initialize Firebase
+});
+
+const Fire = firebase;
+
+export default Fire;
